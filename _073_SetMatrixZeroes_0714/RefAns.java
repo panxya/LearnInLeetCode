f3f53e388cdf8a9a6777979184c15dff14bb415e
panxya <pxy6032@163.com>
2019-07-14 23:03:13 +0800

    update

---
+++ _073_SetMatrixZeroes_0714/RefAns.java
@@ -0,0 +1,39 @@
+package _073_SetMatrixZeroes_0714;
+
+public class RefAns {
+	public static void main(String[] args) {
+		int[][] matrix = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+		setZeroes(matrix);
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.printf("%2d", matrix[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	public static void setZeroes(int[][] matrix) {
+		int col0 = 1, rows = matrix.length, cols = matrix[0].length;
+		for (int i = 0; i < rows; i++) {
+			if (matrix[i][0] == 0) {
+				col0 = 0;
+			}
+			for (int j = 1; j < cols; j++) {
+				if (matrix[i][j] == 0) {
+					matrix[i][0] = matrix[0][j] = 0;
+				}
+			}
+		}
+
+		for (int i = rows - 1; i >= 0; i--) {
+			for (int j = cols - 1; j >= 1; j--) {
+				if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+					matrix[i][j] = 0;
+				}
+			}
+			if (col0 == 0) {
+				matrix[i][0] = 0;
+			}
+		}
+	}
+}

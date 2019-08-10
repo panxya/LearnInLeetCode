dbe48dd0e92c8f61287e27db9955366b26ed9d68
panxya <pxy6032@163.com>
2019-08-10 11:21:27 +0800

    update

---
+++ _100_SameTree_0810/SameTree.java
@@ -0,0 +1,17 @@
+package _100_SameTree_0810;
+
+import _094_BinaryTreeInorderTraversal_0807.TreeNode;
+
+public class SameTree {
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null)
+			return true;
+		if (p == null || q == null)
+			return false;
+		if (p.val == q.val) {
+			return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+		}
+		return false;
+
+	}
+}
